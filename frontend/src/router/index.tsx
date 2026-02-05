2e4557b893e3189d6a85f34c7137f2d070be751d
smw0807 <smw0807@gmail.com>
2026-02-05 19:47:19 +0900

    feat:마이페이지 추가

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -3,6 +3,7 @@ import UserLayout from "@/layouts/UserLayout";
 import Home from "@/pages";
 import CreateAuction from "@/pages/auction/CreateAuction";
 import AuctionDetail from "@/pages/auction/Detail";
+import MyPage from "@/pages/mypage/MyPage";
 import { Route, Routes } from "react-router";
 export default function Router() {
   return (
@@ -13,6 +14,7 @@ export default function Router() {
         <Route element={<UserLayout />}>
           <Route path="/auction/create" element={<CreateAuction />} />
           <Route path="/auctions/:id" element={<AuctionDetail />} />
+          <Route path="/mypage" element={<MyPage />} />
         </Route>
       </Route>
     </Routes>
